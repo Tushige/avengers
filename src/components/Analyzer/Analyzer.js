04db14520854b74d1e60374553bf89c00d2a254e
Tushig <tushige2000@gmail.com>
2020-01-09 13:13:23 -0600

    checkpoint: can guess captain america or iron man and show the result

--- src/components/Analyzer/Analyzer.js
+++ src/components/Analyzer/Analyzer.js
@@ -4,17 +4,20 @@ import './analyzer.scss';
 import axios from 'axios';
 import anime from 'animejs/lib/anime.es.js';
 import ArcReactor from '../ArcReactor/ArcReactor';
+import iron_man from '../../assets/images/iron_man.png';
+import captain_america from '../../assets/images/captain_america.png';
 const DEFAULT_IMAGE_WIDTH = 500;
 const DEFAULT_IMAGE_HEIGHT = 350;
 
 function Analyzer(props) {
   const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [guess, setGuess] = useState('');
+  const [imageDOM, setImageDOM] = useState('blank.gif');
   // the image file
   const [file, setFile] = useState(null);
   // the guess for the image
-  const [heroName, setHeroName] = useState(null);
   // reference to the div whose background will be set to the uploaded image
-  const imageRef = React.createRef();
+  let imageRef = React.createRef();
   const [tl, setTl] = useState(null);
 
   useEffect(() => {
@@ -25,6 +28,7 @@ function Analyzer(props) {
 
   function handleFileUpload(e) {
     const image = e.target.files[0];
+    setGuess('');
     // we first restore the image container to its original size.
     // otherwise, subsequent uploads cannot be larger than previous uploads, so our images can keep shrinking - we don't want that
     anime({
@@ -33,8 +37,12 @@ function Analyzer(props) {
       height: DEFAULT_IMAGE_HEIGHT,
       duration: 700,
       complete: () => {
-        imageRef.current.src = URL.createObjectURL(image);
-        imageRef.current.onload = (e) => {
+        // imageRef.current.src = URL.createObjectURL(image);
+        // imageRef.current.onload = (e) => {
+        console.log(imageDOM)
+        imageDOM.src = URL.createObjectURL(image);
+        imageDOM.onload = (e) => {
+          console.log('image finished loading')
           anime({
             targets: '.image-container',
             width: e.target.width,
@@ -74,39 +82,49 @@ function Analyzer(props) {
 
     setIsAnalyzing(true);
 
-    // axios.post('https://avengers-rend.onrender.com/analyze', formData)
-    //   .then(({ data }) => {
-    //     setIsAnalyzing(false);
-    //     console.log(res);
-    //     setHeroName(data.result);
-    //   })
-    //   .catch(e => console.error(e))
-    setTimeout(() => {
-      setIsAnalyzing(false);
-    }, 5000)
+    axios.post('https://avengers-rend.onrender.com/analyze', formData)
+      .then(({ data }) => {
+        setIsAnalyzing(false);
+        setGuess(data.result);
+      })
+      .catch(e => console.error(e))
+    // setTimeout(() => {
+    //   setIsAnalyzing(false);
+    //   setGuess('iron man')
+    // }, 5000)
   }
   return (
-    <>
-      <div className="button-container">
-        <h1 className="button-title">GUESS</h1>
-        <div className="arc-reactor" onClick={guessCharacter}>
-          <ArcReactor isActive={isAnalyzing} />
-        </div>
+    <div className="analyzer-container">
+      <div className={`captain_america ${guess === 'captain_america' ? 'active' : ''}`}>
+        <div className="glow-left"></div>
+        <img src={captain_america} alt="captain america" width="700px" height="438px" />
       </div>
+      <div className="middle">
+        <div className="button-container">
+          <h1 className="button-title">GUESS</h1>
+          <div className="arc-reactor" onClick={guessCharacter}>
+            <ArcReactor isActive={isAnalyzing} />
+          </div>
+        </div>
 
-      <div className="upload-image-container"
-      >
-        <div className="image-container">
-          <img id="upload-image" ref={imageRef} src="blank.gif" alt="" onError="this.style.display='none'" />
-        </div>
-        <input type="file" id="image-file" onChange={handleFileUpload}>
-        </input>
-        <label htmlFor="image-file" className="upload-label">
-          Upload an image
+        <div className="upload-image-container"
+        >
+          <div className="image-container">
+            <img id="upload-image" ref={(i => setImageDOM(i))} alt="" onError="this.style.display='none'" />
+          </div>
+          <input type="file" id="image-file" onChange={handleFileUpload}>
+          </input>
+          <label htmlFor="image-file" className="upload-label">
+            Upload an image
             <div className="fa fa-upload upload-icon" />
-        </label>
+          </label>
+        </div>
       </div>
-    </>
+      <div className={`iron_man ${guess === 'iron_man' && 'active'}`}>
+        <div className="glow-right"></div>
+        <img src={iron_man} alt="iron man" width="700px" height="393px" />
+      </div>
+    </div>
   )
 }
 export default Analyzer;
